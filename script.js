a9d87a6f1a78c59e4dde0f82fb4d91e6ec76a391
harikrishnakolupula <168351212+harikrishnakolupula@users.noreply.github.com>
2024-09-11 18:04:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showHome() {
+    document.getElementById('homeContent').style.display = 'block';
+    document.getElementById('itemsContent').style.display = 'none';
+}
+
+function showItems() {
+    document.getElementById('homeContent').style.display = 'none';
+    document.getElementById('itemsContent').style.display = 'block';
+}
+
+function search() {
+    alert('Search functionality to be implemented.');
+}
